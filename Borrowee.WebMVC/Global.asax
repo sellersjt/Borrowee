c43406b451f52797eb7b174fe8dacaf0eebcdd3e
sellersjt <jonnie.sellers@gmail.com>
2020-12-12 20:07:12 -0500

    Add project files.

---
+++ Borrowee.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Borrowee.WebMVC.MvcApplication" Language="C#" %>
---
+++ Borrowee.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Borrowee.WebMVC.Startup))]
+namespace Borrowee.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
