1e1ba11c814da41aa2b5ef219378d5956b915085
sellersjt <jonnie.sellers@gmail.com>
2020-12-24 21:25:59 -0500

    update to EntityFramework.6.4.4 for WebMVC

---
+++ Borrowee.Data/Migrations/202012250218527_BackOutImageMapping.cs
@@ -0,0 +1,36 @@
+﻿namespace Borrowee.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class BackOutImageMapping : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.ItemImageMapping", "ItemId", "dbo.Item");
+            DropForeignKey("dbo.ItemImageMapping", "ItemImageId", "dbo.ItemImage");
+            DropIndex("dbo.ItemImageMapping", new[] { "ItemId" });
+            DropIndex("dbo.ItemImageMapping", new[] { "ItemImageId" });
+            DropTable("dbo.ItemImageMapping");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.ItemImageMapping",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        OwnerId = c.Guid(nullable: false),
+                        ItemId = c.Int(nullable: false),
+                        ItemImageId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateIndex("dbo.ItemImageMapping", "ItemImageId");
+            CreateIndex("dbo.ItemImageMapping", "ItemId");
+            AddForeignKey("dbo.ItemImageMapping", "ItemImageId", "dbo.ItemImage", "Id", cascadeDelete: true);
+            AddForeignKey("dbo.ItemImageMapping", "ItemId", "dbo.Item", "Id", cascadeDelete: true);
+        }
+    }
+}
