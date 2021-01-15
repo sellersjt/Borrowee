c5d7150049b31f47174519c54bd1c1ad8d32e687
sellersjt <jonnie.sellers@gmail.com>
2021-01-14 21:14:03 -0500

    Add WebAPI and controller to toggle IsReturned and set ReturnedDate

--- Borrowee.WebMVC/Global.asax.cs
+++ Borrowee.WebMVC/Global.asax.cs
@@ -1,3 +1,4 @@
+using Borrowee.WebMVC.App_Start;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,7 @@ namespace Borrowee.WebMVC
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            WebApiConfig.Register();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
